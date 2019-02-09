b316183f0c62c3840ec9b2dae453ac8e13343af9
tylerstephens814 <tylerstephens814@gmail.com>
2019-02-09 14:57:06 -0700

    Add vote class, add alive field to an idea

--- darwin/models.py
+++ darwin/models.py
@@ -25,7 +25,12 @@ class Idea(models.Model):
     owner = models.ForeignKey("User", related_name='ideas_owned', on_delete=models.CASCADE) #all ideas owned by a user will be deleted when that user is deleted.
     users = models.ManyToManyField("User", related_name='ideas_liked', verbose_name="users who are voting for this idea")
     board = models.ForeignKey('Board', related_name='ideas', on_delete=models.CASCADE) #all ideas on a board will be deleted when that board is deleted.
+    alive = models.BooleanField(default=True)
 
 class Board(models.Model):
     name = models.CharField(max_length=255)
     owner = models.ForeignKey('User', related_name='boards', on_delete=models.CASCADE) #all boards owned by user will be deleted when that user is deleted.
+
+class Vote(models.Model):
+    idea = models.ForeignKey("Idea", related_name="votes", on_delete=models.CASCADE)
+    user = models.ForeignKey("User", related_name="votes", on_delete=models.CASCADE)
