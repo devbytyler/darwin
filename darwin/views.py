3ff00d8cf21908e23baea5bb8b1c716bb012691f
tylerstephens814 <tylerstephens814@gmail.com>
2019-03-07 17:43:54 -0700

    Allow anonymous to register

--- darwin/views.py
+++ darwin/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, generics, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.authtoken import views
@@ -52,6 +52,7 @@ class Login(ObtainAuthToken):
         })
 
 @api_view(['POST'])
+@permission_classes((AllowAny, ))
 def register(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
