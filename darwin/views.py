f0a2136c7310c50ad47c5a742fa2b380868d8596
tylerstephens814 <tylerstephens814@gmail.com>
2019-04-04 20:36:16 -0600

    stuff

--- darwin/views.py
+++ darwin/views.py
@@ -125,6 +125,9 @@ class CommentsList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentModelSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 @api_view(['GET'])
 def user_boards(request, user_id):
     if request.method == 'GET':
